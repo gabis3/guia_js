68c9d0ad8ad69fc958ca65728f34bd559a8e2d16
gabis3 <gabisousaf@outlook.com>
2023-10-17 11:46:58 -0300

    operadores lógicos

--- Js/Fundamentos/dicas.js
+++ Js/Fundamentos/dicas.js
@@ -535,4 +535,22 @@
 // console.log("11) ", undefined === null);
 
 
+// /* - - - - - - - - - - - - Operadores: logicos - - - - - - - - - - - - */
 
+// function compras(trab1, trab2) {
+//     const comprarSorvete = trab1 || trab2; //primeiro ou segundo
+//     const comprarTv50 = trab1 && trab2; // primeiro e segundo
+//     //const comprarTv30 = !!(trab1 ^ trab2); bitwise ou exclusivo xor
+//     const comprarTv30 = trab1 != trab2; // ou exclusivo 
+//     const manterSaudavel = !comprarSorvete;
+
+//     return {comprarSorvete, comprarTv50, comprarTv30, manterSaudavel}
+// }
+
+// console.log(compras(true, true));
+// console.log(compras(true, false));
+// console.log(compras(false, true));
+// console.log(compras(false, false));
+
+
+/* - - - - - - - - - - - - Operadores: unarios - - - - - - - - - - - - */
