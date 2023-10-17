b8624f062e950c6ef8b5f220d04ad56f20e96303
Gabriella Sousa <gabisousaf@outlook.com>
2023-10-17 17:21:19 -0300

    Operadores unários

--- Js/Fundamentos/dicas.js
+++ Js/Fundamentos/dicas.js
@@ -553,4 +553,15 @@
 // console.log(compras(false, false));
 
 
-/* - - - - - - - - - - - - Operadores: unarios - - - - - - - - - - - - */
+// /* - - - - - - - - - - - - Operadores: unarios - - - - - - - - - - - - */
+
+// let num1 = 1
+// let num2 = 4
+
+// num1++
+// ++num1 //tem prioridade - acontece primeiro
+
+// console.log(++num1 === num2--);
+// console.log(num1 === num2);
+
+
