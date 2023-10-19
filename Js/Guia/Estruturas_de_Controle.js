e17b17e6497c5e1fd4af66abfa79e7c492d6d236
gabis3 <gabisousaf@outlook.com>
2023-10-19 20:47:20 -0300

    while

--- Js/Guia/Estruturas_de_Controle.js
+++ Js/Guia/Estruturas_de_Controle.js
@@ -49,3 +49,21 @@
 // notasAlunos(7.9);
 
 
+// /* - - - - - - - - - - - - While - - - - - - - - - - - - */
+// //n sabe quantas vezes vai repetir
+
+// function getInteiroAleatorioEntre(min, max) {
+//     const valor = Math.random() * (max - min) + min;
+//     return Math.floor(valor)
+// }
+
+// let opcao = 0;
+
+// while (opcao != -1 ) {
+//     opcao = getInteiroAleatorioEntre(-1, 10);
+//     console.log(`Opção escolida foi ${opcao}.`);
+// }
+
+// console.log(`Até a próxima`);
+
+
