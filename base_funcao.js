faf27339aefd4a42876f354978e84e71bcfe79f4
Gabriella Sousa <gabisousaf@outlook.com>
2023-10-26 17:23:53 -0300

    Função Anônima

--- base_funcao.js
+++ base_funcao.js
@@ -82,3 +82,32 @@
 // compararComThisArrow(obj);
 
 
+// /* - - - - - - - - - Função Anonima - - - - - - - - - - */
+
+// const soma = function (x, y) {
+//     return x + y;
+// }
+
+// const imprimirResultado = function (a, b, operacao = soma) {
+//    console.log(operacao(a, b));
+// }
+
+// imprimirResultado(2, 4);
+// imprimirResultado(2, 4, function (a, b) {
+//     return a - b;
+// });
+// imprimirResultado(2, 4, (x, y) => x *y)
+// console.log();
+ 
+
+// const pessoa = {
+//     falar: function (){
+//         console.log("Opa");
+//     },
+//     falar2() {
+//         console.log("Opa2");
+//     }
+//  }
+
+// pessoa.falar();
+// pessoa.falar2();
