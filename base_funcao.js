18dc23bdce403b680198a45f5b0d3424d121a705
gabis3 <gabisousaf@outlook.com>
2023-11-05 11:20:09 -0300

    Função Construtora

--- base_funcao.js
+++ base_funcao.js
@@ -98,7 +98,7 @@
 // });
 // imprimirResultado(2, 4, (x, y) => x *y)
 // console.log();
- 
+
 
 // const pessoa = {
 //     falar: function (){
@@ -139,4 +139,37 @@
 // //com callback
 // const notasBaixas2 = notas.filter(notas => notas < 7)
 // console.log(notasBaixas2);
+
+
+// /* - - - - - - - - - Função Construtoras - - - - - - - - - - */
+
+// function Carro(velocidadeMaxima = 200, delta = 5) {
+//   //atributo privado
+//   let velocidadeAtual = 0;
+
+//   //metodo publico
+//   this.acelerar = function () {
+//     if (velocidadeAtual + delta <= velocidadeMaxima) {
+//         velocidadeAtual += delta;
+//     } else {
+//         velocidadeAtual = velocidadeMaxima;
+//     }
+//   }
+
+//   //metodo publico
+//   this.getVelocidadeAtual = function () {
+//     return velocidadeAtual;
+//   }
+// }
+
+// const uno = new Carro;
+// uno.acelerar();
+// console.log(uno.getVelocidadeAtual());
+
+// const ferrari = new Carro(350, 20);
+// ferrari.acelerar();
+// ferrari.acelerar();
+// ferrari.acelerar();
+// console.log(ferrari.getVelocidadeAtual());
+
 
