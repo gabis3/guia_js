60c3143d2cf852dc5a7f1d915de0f0fd177879ba
gabis3 <gabisousaf@outlook.com>
2023-11-05 11:35:17 -0300

    Contexto Léxico

--- base_funcao.js
+++ base_funcao.js
@@ -173,3 +173,17 @@
 // console.log(ferrari.getVelocidadeAtual());
 
 
+// /* - - - - - - - - - Contexto Léxico - - - - - - - - - - */
+
+// const valor = "Global";
+
+// function minhaFuncao() {
+//   console.log(valor);
+// }
+
+// function exec() {
+//   const valor = "Local";
+//   minhaFuncao();
+// }
+
+// exec();
