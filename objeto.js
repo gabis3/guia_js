98743701164441ef792278471935ddd8c0b822f8
gabis3 <gabisousaf@outlook.com>
2024-02-05 17:09:16 -0300

    herança

--- objeto.js
+++ objeto.js
@@ -63,5 +63,138 @@
 // console.log('\n', obj);
 
 
-/* - - - - - - - - - - - - Herança- - - - - - - - - - - - - */
+// /* - - - - - - - - - - - - Herança- - - - - - - - - - - - - */
+
+// const ferrari = {
+//     modelo: "F40",
+//     velMax: 340
+// }
+
+// const volvo = {
+//     modelo: "V40",
+//     velMax: 200
+// }
+
+// console.log(ferrari.prototype);
+// console.log(ferrari.__proto__);
+// console.log(ferrari.__proto__ === Object.prototype);
+// console.log(volvo.__proto__ === Object.prototype);
+// console.log(Object.prototype.__proto__ === null, '\n');
+
+// function MeuObjeto() {}
+// console.log(typeof Object, typeof MeuObjeto);
+// console.log(MeuObjeto.prototype, '\n');
+
+
+// // cadeia de prototios (prototype chain)
+// Object.prototype.atb0 = 0; //não faça isso.
+// const avo = { atb1: "A" }
+// const pai = { __proto__: avo, atb2: "B", atb3: 3 }
+// const filho = { __proto__: pai, atb3: "C" }
+// console.log(filho.atb0, filho.atb1, filho.atb2, filho.atb3, '\n');
+
+// const carro = {
+//     velAtual: 0,
+//     velMax: 200,
+//     acelerarMais(delta) {
+//         if (this.velAtual + delta <= this.velMax) {
+//             this.velAtual += delta;
+//         } else {
+//             this.velAtual = this.velMax
+//         }
+//     },
+//     status() {
+//         return `${this.velAtual}Km/h de ${this.velMax}Km/h`
+//     }
+// }
+
+// const bmw = {
+//     modelo: "F40",
+//     velMax: 340 //shadowing
+// }
+
+// const mercedes = {
+//     modelo: "V40",
+//     status() {
+//         return `${this.modelo}: ${super.status()}`
+//     }
+// }
+
+// Object.setPrototypeOf(bmw, carro);
+// Object.setPrototypeOf(mercedes, carro);
+
+// carro.acelerarMais(40);
+// console.log(bmw.status());
+
+// carro.acelerarMais(300);
+// console.log(mercedes.status(), '\n');
+
+
+// //
+// const dad = { nome: "Pedro", corCabelo: "Preto" }
+// const daughter1 = Object.create(dad);
+// daughter1.nome = "Ana";
+// console.log(`${daughter1.nome} tem cabelo ${daughter1.corCabelo}`);
+
+// const daughter2 = Object.create(dad, {
+//     nome: { value: "Bia", writable: false, enumerable: false}
+// })
+
+// daughter2.nome ="Carla";
+// console.log(`${daughter2.nome} tem cabelo ${daughter2.corCabelo}`);
+
+// console.log(Object.keys(daughter1));
+// console.log(Object.keys(daughter2), '\n');
+
+
+// //
+// function meuObjeto() {}
+// console.log(meuObjeto.prototype);
+
+// const obj1 = new meuObjeto;
+// console.log(obj1.__proto__ === meuObjeto.prototype);
+
+// meuObjeto.prototype.nome = "Anonimo"
+// meuObjeto.prototype.falar = function() {
+//     console.log(`Bom dia! Meu nome é ${this.nome}`);
+// }
+
+// obj1.falar()
+// obj1.nome = "Gabi"
+// obj1.falar()
+
+// const obj2 = {}
+// obj2.__proto__ = meuObjeto.prototype;
+// obj2.nome = "obj2";
+// obj2.falar()
+// console.log();
+
+
+// //
+// String.prototype.reverter = function() {
+//     return this.split('').reverse().join('');
+// }
+// console.log('Gabriella Sousa'.reverter());
+
+// Array.prototype.primeiro = function() {
+//     return this[0];
+// }
+// console.log([1, 2, 3, 4].primeiro(), '\n');
+
+// // String.prototype.toString = function() {
+// //     return "Lascou tudo"
+// // }
+// // console.log('Gabriella Sousa'.reverter());
+
+
+// //
+// function aula(nome, videoId) {
+//     this.nome = nome,
+//     this.videoId = videoId
+// }
+
+// const aula1 = new aula("Bem vindo", 123);
+// const aula2 = new aula("Até breve", 456);
+// console.log(aula1, aula2, '\n');
+
 
